55fda722e3944d783d7c097c6939d9b1d2c0ebe8
ambientaardvark <42986610+ambientaardvark@users.noreply.github.com>
2019-11-21 10:09:35 -0500

    updated action to make enpdateUnit

--- game.js
+++ game.js
@@ -2,11 +2,8 @@
 //input int id, str type, arr action
 //call appropriate update function based on type
 function updateEnt(id, type, action){
-    if(type == "worker"){
-        updateWorker(id, action);
-    }
-    if(type == "fighter"){
-        updateFighter(id, action);
+    if(type == "unit"){
+        updateUnit(id, action);
     }
     if(type == "house"){
         updateHouse(id, action);
@@ -18,36 +15,8 @@ function updateEnt(id, type, action){
 
 //input int id, arr action
 //update ent list based on action
-function updateWorker(id, action){
-    if(action[0] == "build"){
-        //build based of action[1]
-    }
-    if(action[0] == "attack"){
-        //attack based on action[1]
-    }
-    if(action[0] == "harvest"){
-        //harvest based on action[1]
-    }
-    if(action[0] == "build"){
-        //build based on action[1]
-    }
-}
-
-//input int id, arr action
-//update ent list based on action
-function updateFighter(id, action){
-    if(action[0] == "build"){
-        //build based of action[1]
-    }
-    if(action[0] == "attack"){
-        //attack based on action[1]
-    }
-    if(action[0] == "harvest"){
-        //harvest based on action[1]
-    }
-    if(action[0] == "build"){
-        //build based on action[1]
-    }
+function updateUnit(id, action){
+    entList[id].setAction(action[0], action[1])
 }
 
 //input int id, arr action
