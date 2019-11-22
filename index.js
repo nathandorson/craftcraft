434196b4d29dad92cbbbe38f0f38eabef81b9a3e
ambientaardvark <42986610+ambientaardvark@users.noreply.github.com>
2019-11-22 09:39:07 -0500

    updated main loop

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var gameUpdateInterval;
+import gameBoard from game.js
 
 function main()
 {
+    gameWorld = gameBoard()
     gameUpdateInterval = setInterval(() => {
-        gameWorld.update();
+        updateGameWorld(gameWorld, inputs, players);
     }, 1000 / 60);
+}
+
+function updateGameWorld(gameWorld, inputs, players){
+    p1 = players[0]
+    p2 = players[1]
+    for(var i = 0; i++; i < inputs.length()){
+        gameWorld.updateEnt(inputs[i]);
+    }
+    //updateClient
 }
 
 main();
